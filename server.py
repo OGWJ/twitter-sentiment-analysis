8d40c38ea0cdabb9cb509ef95734dbb8f8a13d35
OGWJ <owenglynwilliamjenkins@gmail.com>
2021-09-26 11:05:25 +0100

    added route for getting past days sentiment for up to 7 days

--- server.py
+++ server.py
@@ -2,11 +2,31 @@ from flask import Flask
 from textblob import TextBlob
 import tweepy
 from dotenv import dotenv_values
+from datetime import datetime, timedelta
 
 
 app = Flask(__name__)
 twitter_api_controller = None
 nlp = None
+
+today = datetime.today().strftime('%Y-%m-%d')
+
+def valid_date(date):
+    return True
+
+def within_seven_days(start, end):
+    return True
+
+def get_days_prior(n_days):
+    dates = [today]
+    for i in range(n_days):
+        current_day = dates[i]
+        yyyy, mm, dd = current_day.split('-')
+        current_day = datetime(int(yyyy), int(mm), int(dd))
+        previous_day = current_day - timedelta(1)
+        dates.append(previous_day.strftime('%Y-%m-%d'))
+
+    return sorted(dates)
 
 
 class Twitter_API_Controller:
@@ -23,12 +43,18 @@ class Twitter_API_Controller:
         auth.set_access_token(self.access_token, self.access_secret)
         return tweepy.API(auth)
 
-    def get_tweets(self, query, count=10):
+    def get_tweets(self, query, count=1, 
+                   since=today,
+                   until=today):
+
         tweets = tweepy.Cursor(self.api.search,
         q=query,
-        lang='en').items(5)
+        # since=since,
+        until=until,
+        result_type='recent',
+        lang='en').items(count)
         return tweets
-    
+
 
 def get_sentiment(tweets):
 
@@ -36,6 +62,8 @@ def get_sentiment(tweets):
     n_tweets = 0
 
     for tweet in tweets:
+
+        print(f'\n{tweet.created_at}\n{tweet.text}')
 
         blob = TextBlob(tweet.text)
         tweet_sentiment = 0
@@ -50,14 +78,35 @@ def get_sentiment(tweets):
     return avg_sentiment_polarity / n_tweets
 
 
-@app.route("/<query>")
-def index(query):
 
-    print('getting tweets containing \'{}\'\n'.format(query))
-    tweets = twitter_api_controller.get_tweets(query)
+# GET today default count is 10
+@app.route("/<query>")
+def index_query(query):
+
+    tweets = twitter_api_controller.get_tweets(query) 
     sentiment = get_sentiment(tweets)
 
     return str(sentiment)
+
+
+# GET past 7 days
+@app.route("/<query>/<n_days>")
+def index_query_n_days(query, n_days):
+
+    n_days = int(n_days)
+
+    if n_days > 7:
+        return 'Error: days prior must be <= 7'
+
+    dates = get_days_prior(n_days)
+    data = []
+
+    for date in dates:
+        tweets = twitter_api_controller.get_tweets(query, until=date)
+        sentiment = get_sentiment(tweets)
+        data.append((date, sentiment))
+
+    return str(data)
 
 
 if __name__ == "__main__":
