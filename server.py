676a1145cf33b0a4cec1fa3a21ffaaa7184db24e
“OGWJ” <owenglynwilliamjenkins@gmail.com>
2021-09-29 12:13:55 +0100

    added handling for no provided data on future route

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
+#from flask_cors import CORS
 from textblob import TextBlob
 import tweepy
 from dotenv import dotenv_values
@@ -15,7 +15,7 @@ from controller import TwitterApiController
 from date_utils import *
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 #app.wsgi_app = QueryChecker(app.wsgi_app)
 
 
@@ -55,7 +55,7 @@ def sentiment_query_n_days(query, n_days):
 @app.route("/future/<query>/<n_days>", methods=['GET'])
 def future_query_n_days(query, n_days):
 
-    # test call with 
+    # test call with
     # curl --header "Content-Type: application/json" \
     # --request GET \
     # --data '{"provided":[{"date":"2021-12-21","sentiment":0.5}]' \
@@ -64,21 +64,28 @@ def future_query_n_days(query, n_days):
     n_days = int(n_days)
     dates = get_days_prior(n_days)
     # check request body for provided existing sentiment data
-    provided_data = request.get_json()['provided']
-    print(provided_data)
-    # NOTE, TODO: check in middleware for valid structure of request body before reaching this point
-    
+
+    provided_data = None
     missing_dates = dates
-    for day in provided_data:
-        if day['date'] in missing_dates:
-            missing_dates.remove(day['date'])
-        else:
-            # is not day in range or valid day?
-            print('invalid day')
+    data = []
 
-    data = [(d['date'], d['sentiment']) for d in provided_data]
+    try:
+        provided_data = request.get_json()['provided']
+        for day in provided_data:
+            if day['date'] in missing_dates:
+                missing_dates.remove(day['date'])
+            else:
+                # is not day in range or valid day?
+                print('invalid day')
+        data = [(d['date'], d['sentiment']) for d in provided_data]
+    except:
+        # no data provided
+        pass
 
-    #patch missing data
+    # NOTE, TODO: check in middleware for valid structure of request body before reaching this point
+
+
+    # patch missing data
     for date in missing_dates:
         tweets = tweepy_controller.get_tweets(query, until=date)
         sentiment = tweepy_controller.get_sentiment(tweets)
@@ -109,10 +116,6 @@ def future_query_n_days(query, n_days):
     return str(completed_data)
 
 
-
-
-
-
 # Error handlling
 @app.errorhandler(exceptions.NotFound)
 def error_404():
@@ -127,6 +130,7 @@ def error_500(err):
 config = dotenv_values('.env')
 tweepy_controller = TwitterApiController(config)
 predictive_model = model.Model(save=True, load=False)
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
